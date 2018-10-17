396c30ef09a01c45dc619727771c0a1a8608d562
971721491 <971721491@qq.com>
2018-10-17 14:39:27 +0800

    add varint

--- engine_java/src/main/java/com/alibabacloud/polar_race/engine/common/utils/ByteToInt.java
+++ engine_java/src/main/java/com/alibabacloud/polar_race/engine/common/utils/ByteToInt.java
@@ -22,4 +22,51 @@ public class ByteToInt {
                 (byte) (a & 0xFF)
         };
     }
+
+
+    //因为offset最多需要26位，所以28位是很足够的
+    public static byte[] intToVarByteArray(int value){
+        byte[] bytes = null;
+        int highBitMask = 0x80;
+
+        if (value < (1 << 7) && value >= 0) {
+            bytes = new byte[1];
+            bytes[0] = (byte) value;
+        }
+        else if (value < (1 << 14) && value > 0) {
+            bytes = new byte[2];
+            bytes[0] = (byte) (value | highBitMask);
+            bytes[1] = (byte) (value >>> 7) ;
+        }
+        else if (value < (1 << 21) && value > 0) {
+            bytes = new byte[3];
+            bytes[0] = (byte) (value | highBitMask);
+            bytes[1] = (byte) (value >>> 7 | highBitMask) ;
+            bytes[2] = (byte) (value >>> 14) ;
+        }
+        else {
+            bytes = new byte[4];
+            bytes[0] = (byte) (value | highBitMask);
+            bytes[1] = (byte) (value >>> 7 | highBitMask) ;
+            bytes[2] = (byte) (value >>> 14 | highBitMask) ;
+            bytes[3] = (byte) (value >>> 21) ;
+        }
+        return bytes;
+    }
+    public static int varByteArrayToInt(byte[] value){
+        int result = 0;
+        for (int shift = 0; shift <= 28; shift += 7) {
+            int b = value[shift/7];
+
+            // add the lower 7 bits to the result
+            result |= ((b & 0x7f) << shift);
+
+            // if high bit is not set, this is the last byte in the number
+            if ((b & 0x80) == 0) {
+                return result;
+            }
+        }
+
+        return result;
+    }
 }
