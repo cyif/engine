809d209055d9788858b920777d08fa139dec380f
971721491 <971721491@qq.com>
2018-10-25 18:54:17 +0800

    add recover log

--- engine_java/src/main/java/com/alibabacloud/polar_race/engine/common/impl/DBImpl.java
+++ engine_java/src/main/java/com/alibabacloud/polar_race/engine/common/impl/DBImpl.java
@@ -58,7 +58,7 @@ public class DBImpl {
                 break;
             int now = this.wrotePosition.getAndAdd(1);//恢复wroteposition
 
-            if (now % 1024*64 == 0)
+            if (now % (1024*64) == 0)
                 System.out.println("recover   " + now);
 
             byte[] key = new byte[8];
