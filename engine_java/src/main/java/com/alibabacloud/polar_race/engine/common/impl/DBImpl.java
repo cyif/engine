fc21306225567c2dfd9f814b81501f1a3c3473e2
971721491 <971721491@qq.com>
2018-10-29 12:35:45 +0800

    lisr

--- engine_java/src/main/java/com/alibabacloud/polar_race/engine/common/impl/DBImpl.java
+++ engine_java/src/main/java/com/alibabacloud/polar_race/engine/common/impl/DBImpl.java
@@ -46,7 +46,7 @@ public class DBImpl {
         File dir = new File(path, "key");
         if (dir.exists()){
             System.out.println("---------------Start read or write append---------------");
-            this.map = new ConcurrencyHashTable(256*1024, 0);
+            this.map = new ConcurrencyHashTable(1024*1024, 0);
             keyLog = new KeyLog(GlobalConfig.KeyFileSize, path);//keylog恢复
             recoverHashtable();//hashtable恢复和wroteposition恢复
             System.out.println("Recover finished");
