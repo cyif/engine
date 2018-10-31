aa684dc9f2a895017817095822f76434ce6e6224
971721491 <971721491@qq.com>
2018-10-31 10:48:56 +0800

    jvm

--- engine_java/src/main/java/com/alibabacloud/polar_race/engine/common/impl/DBImpl.java
+++ engine_java/src/main/java/com/alibabacloud/polar_race/engine/common/impl/DBImpl.java
@@ -36,7 +36,7 @@ public class DBImpl {
         File dir = new File(path, "key");
         if (dir.exists()){
             System.out.println("---------------Start read or write append---------------");
-            tmap = new TLongIntHashMap(64*1024*1024, 1.0F, 0L, -1);
+            tmap = new TLongIntHashMap(64000000, 1.0F, 0L, -1);
             keyLog = new KeyLog(GlobalConfig.KeyFileSize, path);//keylog恢复
             recoverHashtable();//hashtable恢复和wroteposition恢复
             System.out.println("Recover finished");
