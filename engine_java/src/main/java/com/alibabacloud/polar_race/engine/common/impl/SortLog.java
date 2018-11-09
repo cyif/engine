d8d86d271fb3adb2cf401e54c9232d9228bdbaa4
971721491 <971721491@qq.com>
2018-11-09 10:30:04 +0800

    试一下

--- engine_java/src/main/java/com/alibabacloud/polar_race/engine/common/impl/SortLog.java
+++ engine_java/src/main/java/com/alibabacloud/polar_race/engine/common/impl/SortLog.java
@@ -95,10 +95,10 @@ public class SortLog {
     //index相当于0-255
     public int find(long key, int index) {
 
-        int left = (int)(((size-1)>>>16) * index * 0.5);
+        int left = (int)(((size-1)>>>8) * index * 0.5);
         if (left < 0)
             left = 0;
-        int right = (int)(((size-1)>>>16) * (index+1) * 2);
+        int right = (int)(((size-1)>>>8) * (index+1) * 2);
         if (right > size-1)
             right = size - 1;
 
@@ -106,6 +106,9 @@ public class SortLog {
         while (left <= right) {
             middle = left + (right - left) / 2;
             if (keyArray[middle] == key) {
+
+                System.out.println("f");
+
                 return offsetArray[middle];
             } else if (key < keyArray[middle]) {
                 right = middle - 1;
