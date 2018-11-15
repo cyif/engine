6101dafc3057aaf502f6593d06ae84c09a0bdcbc
qwc <971721491@qq.com>
2018-11-15 21:15:18 +0800

    cpp final

--- engine_race/PEngine.h
+++ engine_race/PEngine.h
@@ -107,7 +107,7 @@ namespace polar_race {
 
 
         int getLogId(const PolarString &k) {
-            return (*((u_int8_t *) (k.data()))) % 4;
+            return (*((u_int8_t *) (k.data()))) >> 2;
         }
 
         void put(const PolarString &key, const PolarString &value) {
