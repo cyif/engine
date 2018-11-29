2f43f00b1e8303e15370fe8cbf41ee09fbc4b9ff
杨煜溟 <yangym@zju.edu.cn>
2018-11-29 14:43:14 +0800

    256 file 16k cache

--- engine_race/params.h
+++ engine_race/params.h
@@ -9,18 +9,18 @@
 
 const int MAX_RANGE_COUNT = 2;
 
-const int LOG_NUM = 2048;
-const int NUM_PER_SLOT = 1024 * 32;
+const int LOG_NUM = 1024;
+const int NUM_PER_SLOT = 1024 * 64;
 const size_t VALUE_LOG_SIZE = NUM_PER_SLOT * 4096;  //128mb
 const size_t KEY_LOG_SIZE = NUM_PER_SLOT * 8;
 
-const size_t FILE_NUM = 512;
+const size_t FILE_NUM = 256;
 
 const size_t CACHE_SIZE = VALUE_LOG_SIZE;
-const int CACHE_NUM = 8;
-const int CACHE_BLOCK_SIZE = 128 * 1024 * 1024;   //16mb
+const int CACHE_NUM = 4;
+const int CACHE_BLOCK_SIZE = VALUE_LOG_SIZE;   //16mb
 
-const size_t PAGE_PER_BLOCK = 8;
+const size_t PAGE_PER_BLOCK = 4;
 const size_t BLOCK_SIZE = PAGE_PER_BLOCK * 4096;
 
 const int RECOVER_THREAD = 64;
