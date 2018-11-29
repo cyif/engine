9b0e630fa216e99cd4fc62a1500f659b201a4e2e
杨煜溟 <yangym@zju.edu.cn>
2018-11-29 21:19:58 +0800

    128 files

--- engine_race/params.h
+++ engine_race/params.h
@@ -14,7 +14,7 @@ const int NUM_PER_SLOT = 1024 * 32;
 const size_t VALUE_LOG_SIZE = NUM_PER_SLOT * 4096;  //128mb
 const size_t KEY_LOG_SIZE = NUM_PER_SLOT * 8;
 
-const size_t FILE_NUM = 256;
+const size_t FILE_NUM = 128;
 
 const size_t CACHE_SIZE = VALUE_LOG_SIZE;
 const int CACHE_NUM = 4;
@@ -23,7 +23,7 @@ const int CACHE_BLOCK_SIZE = VALUE_LOG_SIZE;   //64mb
 const size_t PAGE_PER_BLOCK = 4;
 const size_t BLOCK_SIZE = PAGE_PER_BLOCK * 4096;
 
-const int RECOVER_THREAD = 128;
+const int RECOVER_THREAD = 64;
 const int READDISK_THREAD = 2;
 
 #endif //ENGINE_PARAMS_H
