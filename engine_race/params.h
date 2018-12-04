69cc43a822f465629940bc774ff79d0121976f50
qwc <971721491@qq.com>
2018-12-04 13:11:15 +0800

    128file  2thread

--- engine_race/params.h
+++ engine_race/params.h
@@ -17,7 +17,7 @@ const int NUM_PER_SLOT = 1024 * 16;
 const size_t VALUE_LOG_SIZE = NUM_PER_SLOT * 4096;  //128mb
 const size_t KEY_LOG_SIZE = NUM_PER_SLOT * 8;
 
-const int FILE_NUM = 64;
+const int FILE_NUM = 128;
 
 const int SORT_LOG_SIZE = NUM_PER_SLOT;
 const int SORT_FILE_SIZE = SORT_LOG_SIZE * (LOG_NUM / FILE_NUM);
@@ -33,7 +33,7 @@ const int PAGE_PER_BLOCK = 4;
 const size_t BLOCK_SIZE = PAGE_PER_BLOCK * 4096;
 
 const int RECOVER_THREAD = 64;
-const int READDISK_THREAD = 3;
+const int READDISK_THREAD = 2;
 
 const int MAX_LENGTH_INSERT_SORT = 12;
 
